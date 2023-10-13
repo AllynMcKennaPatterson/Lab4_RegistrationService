75d34325a66b94c9d7500ead915162a34397e2cb
G00392765@atu.ie <G00392765@atu.ie>
2023-10-13 17:15:27 +0100

    Updated URIs

--- src/main/java/at/cicd2/lab4_registrationservice/NotificationServiceClient.java
+++ src/main/java/at/cicd2/lab4_registrationservice/NotificationServiceClient.java
@@ -4,8 +4,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "notification", url = "http://localhost:8083")
+@FeignClient(name = "notification-service", url = "http://localhost:8083")
 public interface NotificationServiceClient {
-    @PostMapping("/confirmRegisterUser")
+    @PostMapping("/notification")
     String someDetails(@RequestBody UserDetails userDetails);
 }
