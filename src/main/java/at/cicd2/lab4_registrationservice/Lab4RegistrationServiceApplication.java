80fcdf9d24d9c4ef25ecfc6767cae522fd6bb40c
G00392765@atu.ie <G00392765@atu.ie>
2023-10-13 17:30:41 +0100

    Added @EnableFeignClients

--- src/main/java/at/cicd2/lab4_registrationservice/Lab4RegistrationServiceApplication.java
+++ src/main/java/at/cicd2/lab4_registrationservice/Lab4RegistrationServiceApplication.java
@@ -2,7 +2,9 @@ package at.cicd2.lab4_registrationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class Lab4RegistrationServiceApplication {
 
